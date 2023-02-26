3057a059065d9732d0f53b22455f5a661da54ddf
mehmet.gunes <mehmet.gunes@alegradigital.com>
2023-02-26 18:29:30 +0300

    upgrade to php8

--- src/routes.php
+++ src/routes.php
@@ -6,19 +6,19 @@ use EscolaLms\Scorm\Http\Controllers\ScormTrackController;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api/admin/scorm', 'middleware' => ['auth:api']], function () {
-    Route::post('/upload', [ScormController::class, "upload"]);
-    Route::post('/parse', [ScormController::class, "parse"]);
-    Route::delete('/{scormModel}', [ScormController::class, "delete"]);
-    Route::get('/', [ScormController::class, "index"]);
-    Route::get('/scos', [ScormController::class, "getScos"]);
-});
-
-Route::group(['prefix' => 'api/scorm'], function () {
-    Route::get('/play/{uuid}', [ScormController::class, "show"]);
-
-    Route::group(['prefix' => '/track', 'middleware' => ['auth:api']], function () {
-        Route::post('/{uuid}', [ScormTrackController::class, 'set']);
-        Route::get('/{scoId}/{key}', [ScormTrackController::class, 'get']);
-    });
-});
+//Route::group(['prefix' => 'api/admin/scorm', 'middleware' => ['auth:api']], function () {
+//    Route::post('/upload', [ScormController::class, "upload"]);
+//    Route::post('/parse', [ScormController::class, "parse"]);
+//    Route::delete('/{scormModel}', [ScormController::class, "delete"]);
+//    Route::get('/', [ScormController::class, "index"]);
+//    Route::get('/scos', [ScormController::class, "getScos"]);
+//});
+//
+//Route::group(['prefix' => 'api/scorm'], function () {
+//    Route::get('/play/{uuid}', [ScormController::class, "show"]);
+//
+//    Route::group(['prefix' => '/track', 'middleware' => ['auth:api']], function () {
+//        Route::post('/{uuid}', [ScormTrackController::class, 'set']);
+//        Route::get('/{scoId}/{key}', [ScormTrackController::class, 'get']);
+//    });
+//});
